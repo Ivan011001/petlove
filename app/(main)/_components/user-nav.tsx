22f95125a5acf36321dab030fbcc2723f608edf7
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-02-07 18:13:07 +0200

    created responsive header

--- app/(main)/_components/user-nav.tsx
+++ app/(main)/_components/user-nav.tsx
@@ -4,21 +4,21 @@ const UserNav = () => {
   return (
     <nav className="justify-start items-start gap-2.5 inline-flex">
       <Link
-        className="px-5 py-[15px] rounded-[30px] border border-neutral-800 border-opacity-20 justify-center items-center gap-2.5 inline-flex"
+        className="hover:border-amber-400 transition-all duration-300 px-5 py-[15px] rounded-[30px] border border-neutral-800 border-opacity-20 justify-center items-center gap-2.5 inline-flex"
         href="/news"
       >
         <p className="text-neutral-800 text-base font-medium leading-tight">News</p>
       </Link>
 
       <Link
-        className="px-5 py-[15px] rounded-[30px] border border-neutral-800 border-opacity-20 justify-center items-center gap-2.5 inline-flex"
+        className=" hover:border-amber-400 transition-all duration-300 px-5 py-[15px] rounded-[30px] border border-neutral-800 border-opacity-20 justify-center items-center gap-2.5 inline-flex"
         href="/news"
       >
         <p className="text-neutral-800 text-base font-medium leading-tight">Find Pet</p>
       </Link>
 
       <Link
-        className="px-5 py-[15px] rounded-[30px] border border-neutral-800 border-opacity-20 justify-center items-center gap-2.5 inline-flex"
+        className="hover:border-amber-400 transition-all duration-300 px-5 py-[15px] rounded-[30px] border border-neutral-800 border-opacity-20 justify-center items-center gap-2.5 inline-flex"
         href="/news"
       >
         <p className="text-neutral-800 text-base font-medium leading-tight">
