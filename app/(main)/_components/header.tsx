93274e64d32819c84515809e33cc5306a8f02e1f
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-02-07 13:12:45 +0200

    fixed layout

--- app/(main)/_components/header.tsx
+++ app/(main)/_components/header.tsx
@@ -1,14 +1,22 @@
+"use client";
+
+import StoreProvider from "@/providers/store-provider";
+
+import Logo from "@/components/logo";
+
 import UserNav from "./user-nav";
 import UserMenu from "./user-menu";
 
 const Header = () => {
   return (
     <header className="w-full flex justify-between items-center py-5 m-auto px-[60px] ">
-      <div>Logo</div>
+      <Logo />
 
       <UserNav />
 
-      <UserMenu />
+      <StoreProvider>
+        <UserMenu />
+      </StoreProvider>
     </header>
   );
 };
---
+++ app/(main)/layout.tsx
@@ -0,0 +1,12 @@
+import Header from "./_components/header";
+
+const MainLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      <Header />
+      {children}
+    </div>
+  );
+};
+
+export default MainLayout;
