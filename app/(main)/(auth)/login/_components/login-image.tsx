727fddf0ebff8845279f4358966bb9e89187946b
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-02-09 18:21:34 +0200

    added login image

---
+++ app/(main)/(auth)/login/_components/login-image.tsx
@@ -0,0 +1,44 @@
+import Image from "next/image";
+
+const LoginImage = () => {
+  return (
+    <div
+      className="w-full h-full bg-center-top bg-cover bg-no-repeat"
+      style={{
+        backgroundImage:
+          "url(https://cdn.animaapp.com/projects/65c4c76946e5c0edf183ef9a/releases/65c4c80c7fc157c12e418353/img/rectangle-4561.svg)",
+      }}
+    >
+      <div
+        className="flex items-end p-8 lg:pl-[61px] lg:pb-[97px] relative bg-top bg-cover bg-no-repeat w-full h-full"
+        style={{
+          backgroundImage:
+            "url(https://cdn.animaapp.com/projects/65c4c76946e5c0edf183ef9a/releases/65c4c80c7fc157c12e418353/img/telegram-cloud-document-2-5460905295997845524-2.png)",
+        }}
+      >
+        <div className="hidden md:flex gap-2 items-start bg-white rounded-3xl w-[294px] p-4">
+          <div className="flex-shrink rounded-full w-[60px] h-[60px] bg-yellow-50 flex justify-center items-center p-4">
+            <Image src="/dog.png" alt="Dog" width={32} height={32} />
+          </div>
+          <div>
+            <div className="flex items-center justify-between mb-2">
+              <h3 className="text-accent text-base font-bold leading-tight">Rich</h3>
+              <p className="text-neutral-800 text-opacity-50 text-xs font-medium  leading-none">
+                Birthday:{" "}
+                <span className="text-neutral-800 text-xs font-medium  leading-none">
+                  21.09.2020
+                </span>
+              </p>
+            </div>
+            <p className="w-48 text-neutral-800 text-opacity-80 text-xs font-medium leading-none">
+              Rich would be the perfect addition to an active family that loves to play
+              and go on walks. I bet he would love having a doggy playmate too!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginImage;
--- app/(main)/(auth)/login/page.tsx
+++ app/(main)/(auth)/login/page.tsx
@@ -1,11 +1,14 @@
 import Link from "next/link";
 
+import LoginImage from "./_components/login-image";
 import LoginForm from "./_components/login-form";
 
 const LoginPage = () => {
   return (
     <div className="container min-h-full flex flex-col lg:flex-row gap-2.5 md:gap-4 lg:gap-8 pb-5">
-      <section className="flex-grow rounded-3xl md:rounded-[60px] bg-accent w-full h-auto lg:h-full"></section>
+      <section className="flex-grow rounded-3xl md:rounded-[60px] bg-accent w-full min-h-[280px] md:min-h-[302px] h-auto lg:h-full">
+        <LoginImage />
+      </section>
       <section className="flex items-center justify-center rounded-3xl md:rounded-[60px] bg-white w-full h-auto lg:h-full py-[60px] md:py-[70px] px-5">
         <div className="lg:max-w-[424px]">
           <h2 className="text-neutral-800 text-3xl md:text-5xl font-bold leading-7 md:leading-10 mb-3 md:mb-4">
