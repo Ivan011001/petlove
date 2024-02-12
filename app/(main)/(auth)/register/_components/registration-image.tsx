182476955c761b6b33cd77c197adc4845fb3d89d
georgijbudik <georgijbudik@gmail.com>
2024-02-12 16:37:50 +0100

    fix registration image position

--- app/(main)/(auth)/register/_components/registration-image.tsx
+++ app/(main)/(auth)/register/_components/registration-image.tsx
@@ -3,40 +3,39 @@ import Image from "next/image";
 const RegistrationImage = () => {
   return (
     <div
-      className="w-full h-full bg-center-top bg-cover bg-no-repeat"
+      className="w-full h-full bg-center-top bg-cover bg-no-repeat relative md:overflow-hidden"
       style={{
         backgroundImage:
           "url(https://cdn.animaapp.com/projects/65c4c76946e5c0edf183ef9a/releases/65c4c80c7fc157c12e418353/img/rectangle-4561.svg)",
       }}
     >
       <div
-        className="flex items-end p-8 lg:pl-[61px] lg:pb-[97px] relative bg-top lg:bg-bottom bg-cover md:bg-[50px] lg:bg-auto bg-no-repeat w-full h-full"
+        className="flex items-end p-8 lg:pl-[61px] lg:pb-[97px] absolute md:right-[90px] lg:bg-bottom bg-cover md:float-end md:top-0 md:w-[364px] md:h-[302px] lg:bg-auto bg-no-repeat w-full h-full lg:h-[632px]"
         style={{
           backgroundImage:
             "url(https://cdn.animaapp.com/projects/65c4c76946e5c0edf183ef9a/releases/65c4c80c7fc157c12e418353/img/telegram-cloud-document-2-5460905295997845524-2.png)",
         }}
-      >
-        <div className="hidden md:flex gap-2 items-start bg-white rounded-3xl w-[294px] p-4">
-          <div className="flex-shrink rounded-full w-[60px] h-[60px] bg-yellow-50 flex justify-center items-center p-4">
-            <Image src="/cat.png" alt="Cat" width={32} height={32} />
-          </div>
-          <div>
-            <div className="flex items-center justify-between mb-2">
-              <h3 className="text-accent text-base font-bold leading-tight">
-                Jack
-              </h3>
-              <p className="text-neutral-800 text-opacity-50 text-xs font-medium  leading-none">
-                Birthday:{" "}
-                <span className="text-neutral-800 text-xs font-medium  leading-none">
-                  18.10.2021
-                </span>
-              </p>
-            </div>
-            <p className="w-48 text-neutral-800 text-opacity-80 text-xs font-medium leading-none">
-              Jack is a gray Persian cat with green eyes. He loves to be
-              pampered and groomed, and enjoys playing with toys.
+      />
+      <div className="hidden md:flex gap-2 items-start bg-white rounded-3xl w-[294px] p-4 md:absolute md:top-[149px] md:left-8">
+        <div className="flex-shrink rounded-full w-[60px] h-[60px] bg-yellow-50 flex justify-center items-center p-4">
+          <Image src="/cat.png" alt="Cat" width={32} height={32} />
+        </div>
+        <div>
+          <div className="flex items-center justify-between mb-2">
+            <h3 className="text-accent text-base font-bold leading-tight">
+              Jack
+            </h3>
+            <p className="text-neutral-800 text-opacity-50 text-xs font-medium  leading-none">
+              Birthday:{" "}
+              <span className="text-neutral-800 text-xs font-medium  leading-none">
+                18.10.2021
+              </span>
             </p>
           </div>
+          <p className="w-48 text-neutral-800 text-opacity-80 text-xs font-medium leading-none">
+            Jack is a gray Persian cat with green eyes. He loves to be pampered
+            and groomed, and enjoys playing with toys.
+          </p>
         </div>
       </div>
     </div>
