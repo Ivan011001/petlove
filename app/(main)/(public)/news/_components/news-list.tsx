e03a54a5a0145e9cf06d376579c1fca6812f4919
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-02-14 13:57:14 +0200

    fixed news item

--- app/(main)/(public)/news/_components/news-list.tsx
+++ app/(main)/(public)/news/_components/news-list.tsx
@@ -10,11 +10,7 @@ const NewsList = ({ news }: INewsListProps) => {
   return (
     <ul className="flex flex-col items-center md:flex-row md:justify-center flex-wrap gap-6 md:gap-x-6 md:gap-y-8 lg:gap-x-[35px] lg:gap-y-[40px]">
       {news.map((item) => {
-        return (
-          <li key={item.id}>
-            <NewsItem newsItem={item} />
-          </li>
-        );
+        return <NewsItem key={item.id} newsItem={item} />;
       })}
     </ul>
   );
