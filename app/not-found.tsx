65241c89a70d1dbbb616950bf6c37eec40de5c1a
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-02-14 11:28:50 +0200

    fixed 404 page styles

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -19,27 +19,28 @@ const NotFound = () => {
     <>
       <Header />
       <main className="flex container pb-5">
-        <div className="container bg-accent rounded-3xl flex flex-col justify-center items-center">
+        <div className="container bg-accent rounded-3xl flex flex-col justify-center items-center px-[33px]">
           <div className="flex flex-col justify-start items-center gap-5 ">
             <div className="flex justify-center items-center gap-2">
-              <div className="text-white text-9xl md:text-[300px] font-extrabold leading-10">
+              <div className="text-white text-7xl sm:text-9xl md:text-[300px] font-extrabold leading-[120px]">
                 4
               </div>
-              <div className="flex bg-white bg-opacity-10 rounded-full justify-end items-center">
+              <div className="flex bg-white bg-opacity-10 rounded-full justify-end items-center h-[80px] w-[80px] sm:h-[110px] sm:w-[110px] md:h-[280px] md:w-[280px]">
                 <Image
-                  className="h-25 w-25 md:h-72 md:w-72"
+                  className="h-[80px] w-[80px] sm:h-[110px] sm:w-[110px] md:h-[280px] md:w-[280px]"
                   src="/not-found.png"
                   alt="Not Found"
-                  width={100}
-                  height={100}
+                  width={280}
+                  height={280}
+                  priority
                 />
               </div>
-              <div className="text-white text-9xl md:text-[300px] font-extrabold leading-10">
+              <div className="text-white text-7xl sm:text-9xl md:text-[300px] font-extrabold leading-[120px]">
                 4
               </div>
             </div>
             <div className="flex-col justify-start items-center gap-5 flex">
-              <p className="text-white text-base md:text-2xl font-bold leading-tight md:leading-7">
+              <p className="text-white text-base md:text-2xl font-bold leading-tight md:leading-7 text-center">
                 Ooops! This page not found :(
               </p>
               <Button
