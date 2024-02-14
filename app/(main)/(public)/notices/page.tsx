f8e648fb5d3c9c966044329c6d7a83616a520aa3
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-02-14 15:30:26 +0200

    created notices filters

--- app/(main)/(public)/notices/page.tsx
+++ app/(main)/(public)/notices/page.tsx
@@ -1,11 +1,16 @@
 import PageContainer from "@/components/page-container";
 import Title from "@/components/title";
 
+import NoticesFilters from "./_components/notices-filters";
+
 const NoticesPage = () => {
   return (
     <div>
       <Title>Find your favorite pet</Title>
-      <div className="sm:w-[335px] h-[378px] bg-[#FFF4DF]"></div>
+      {/* sm:w-[335px] h-[378px] */}
+      <div className="p-5 md:py-10 md:px-8 lg:px-10 rounded-3xl bg-[#FFF4DF]">
+        <NoticesFilters />
+      </div>
     </div>
   );
 };
