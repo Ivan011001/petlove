8f2d728b6ef173a64f2b3a37779d939480c7bdbd
georgijbudik <georgijbudik@gmail.com>
2024-02-14 23:02:29 +0100

    fix

--- app/(main)/(public)/layout.tsx
+++ app/(main)/(public)/layout.tsx
@@ -2,7 +2,7 @@ import PageContainer from "@/components/page-container";
 
 const PublicLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <section className="container flex flex-col pb-[80px]">
+    <section className="container flex flex-col pb-[80px] px-5">
       <PageContainer>{children}</PageContainer>
     </section>
   );
