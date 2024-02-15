8f4f2cd5af8b1609d2c3fd922faa7ac22feb4aab
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-02-15 14:19:54 +0200

    deleted metainfo

--- app/(main)/(public)/news/layout.tsx
+++ app/(main)/(public)/news/layout.tsx
@@ -1,9 +1,3 @@
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "News",
-};
-
 const NewsLayout = ({ children }: { children: React.ReactNode }) => {
   return children;
 };
--- app/(main)/(public)/notices/layout.tsx
+++ app/(main)/(public)/notices/layout.tsx
@@ -1,9 +1,3 @@
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Notices",
-};
-
 const NoticesLayout = ({ children }: { children: React.ReactNode }) => {
   return children;
 };
