7e871f658e95f1c9a78de29f573553324e98624e
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-02-16 13:43:05 +0200

    created addpet page

---
+++ app/(main)/add-pet/layout.tsx
@@ -0,0 +1,5 @@
+const AddPetLayout = ({ children }: { children: React.ReactNode }) => {
+  return children;
+};
+
+export default AddPetLayout;
---
+++ app/(main)/add-pet/page.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+
+import AddPetForm from "./_components/add-pet-form";
+import AddPetImage from "./_components/add-pet-image";
+
+const AddPetPage = () => {
+  return (
+    <div className="container min-h-full flex flex-col lg:flex-row gap-2.5 md:gap-4 lg:gap-8 pb-5">
+      <section className="h-[280px] flex-grow rounded-3xl md:rounded-[60px] bg-accent w-full sm:max-w-[335px] md:h-[302px] md:max-w-[704px] lg:h-full mx-auto">
+        <AddPetImage />
+      </section>
+      <section className="flex items-center justify-center rounded-3xl md:rounded-[60px] bg-white w-full h-auto lg:h-full p-5 pt-7 sm:max-w-[335px] mx-auto md:max-w-[704px]">
+        <div className="lg:max-w-[424px]">
+          <h2 className="text-neutral-800 text-3xl md:text-5xl font-bold leading-7 md:leading-10 mb-6 md:mb-4">
+            Add my pet/{" "}
+            <span className="text-zinc-800 text-opacity-40 text-sm font-bold leading-none">
+              Personal details
+            </span>
+          </h2>
+
+          <AddPetForm />
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default AddPetPage;
