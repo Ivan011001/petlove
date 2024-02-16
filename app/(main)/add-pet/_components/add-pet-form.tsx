c25f3f7d50bf99e5b7a904e5cf7be782e3d850b9
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-02-16 13:59:28 +0200

    added tablet styles

--- app/(main)/add-pet/_components/add-pet-form.tsx
+++ app/(main)/add-pet/_components/add-pet-form.tsx
@@ -41,35 +41,35 @@ const AddPetForm = () => {
       {({ errors, touched, isValid, dirty }) => (
         <Form className="flex flex-col">
           <ul className="flex gap-2 items-center mb-2">
-            <li className="w-8 h-8 p-1.5 bg-rose-500 bg-opacity-10 rounded-full flex justify-center items-center">
+            <li className="w-8 h-8 md:w-10 md:h-10 p-1.5 bg-rose-500 bg-opacity-10 rounded-full flex justify-center items-center">
               <svg className="stroke-rose-500">
-                <use xlinkHref="/sprite.svg#icon-male"></use>
+                <use xlinkHref="/sprite.svg#icon-female"></use>
               </svg>
             </li>
 
-            <li className="w-8 h-8 p-1.5 bg-blue-400 bg-opacity-10 rounded-full flex justify-center items-center">
+            <li className="w-8 h-8 md:w-10 md:h-10 p-1.5 bg-blue-400 bg-opacity-10 rounded-full flex justify-center items-center">
               <svg className="stroke-blue-400">
                 <use xlinkHref="/sprite.svg#icon-male"></use>
               </svg>
             </li>
 
-            <li className="w-8 h-8 p-1.5 bg-yellow-50 rounded-full flex justify-center items-center">
+            <li className="w-8 h-8 md:w-10 md:h-10 p-1.5 bg-yellow-50 rounded-full flex justify-center items-center">
               <svg className="fill-accent">
                 <use xlinkHref="/sprite.svg#icon-duomale"></use>
               </svg>
             </li>
           </ul>
-          <div className="h-[68px] w-[68px] rounded-full bg-yellow-50 self-center flex justify-center items-center mb-4">
-            <svg className="h-[34px] w-[34px] fill-accent">
+          <div className="h-[68px] w-[68px] md:h-[86px] md:w-[86px] rounded-full bg-yellow-50 self-center flex justify-center items-center mb-4 md:mb-3">
+            <svg className="h-[34px] w-[34px] md:w-[44px] md:h-[44px] fill-accent">
               <use xlinkHref="/sprite.svg#icon-paw"></use>
             </svg>
           </div>
-          <div className="flex flex-col gap-2.5 mb-[30px]">
+          <div className="flex flex-col gap-2.5 md:gap-4 mb-[30px]">
             <div>
               <div className="flex gap-2">
                 <Field
                   className={cn(
-                    "w-full py-[9px] px-3 lg:py-4 lg:px-4 border border-neutral-800 border-opacity-20 transition-all duration-300 outline-none focus:border-accent placeholder:text-sm md:placeholder:text-base",
+                    "w-full py-[9px] px-3 md:py-3 lg:py-4 lg:px-4 border border-neutral-800 border-opacity-20 transition-all duration-300 outline-none focus:border-accent placeholder:text-sm md:placeholder:text-base",
                     touched.imgURL
                       ? errors.imgURL
                         ? "border-red-500 border-opacity-100"
@@ -80,8 +80,8 @@ const AddPetForm = () => {
                   name="imgURL"
                   placeholder="Enter URL"
                 />
-                <Button className="group p-[10px] bg-yellow-50 rounded-3xl text-neutral-800 text-xs font-medium leading-none flex gap-1 items-center hover:text-white hover:bg-accent">
-                  <span>Upload photo</span>
+                <Button className="group p-[10px] md:px-4 bg-yellow-50 rounded-3xl text-neutral-800 font-medium leading-none flex gap-1 md:gap-2 items-center hover:text-white hover:bg-accent">
+                  <span className="text-sx md:text-sm">Upload photo</span>
                   <svg className="w-4 h-4 fill-none stroke-accent group-hover:stroke-white transition-all duration-300">
                     <use xlinkHref="/sprite.svg#icon-upload"></use>
                   </svg>
@@ -164,14 +164,14 @@ const AddPetForm = () => {
           <div className="flex justify-end gap-2 items-center">
             <Button
               type="submit"
-              className="py-4 lg:py-4 bg-neutral-800 bg-opacity-5 text-neutral-800 hover:bg-accent hover:text-yellow-50"
+              className="md:w-44 py-3 lg:py-4 bg-neutral-800 bg-opacity-5 text-neutral-800 hover:bg-accent hover:text-yellow-50"
             >
               <Link href="/">Back</Link>
             </Button>
             <Button
               disabled={!isValid || !dirty}
               type="submit"
-              className="py-4 lg:py-4"
+              className="md:w-44 py-3 lg:py-4"
             >
               Submit
             </Button>
