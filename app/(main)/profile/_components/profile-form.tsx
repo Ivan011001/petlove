0c4ed67422eb78f9b4129fd15e3b769dbb88cf05
georgijbudik <georgijbudik@gmail.com>
2024-02-18 00:48:01 +0100

    fix profile-form

--- app/(main)/profile/_components/profile-form.tsx
+++ app/(main)/profile/_components/profile-form.tsx
@@ -1,25 +1,9 @@
 import { selectUserEmail, selectUserName } from "@/state/auth/authSelectors";
 import { useAppSelector } from "@/state/hooks";
 
-const inputs = [
-  {
-    name: "name",
-    placeholder: "Name",
-    selector: selectUserName,
-  },
-  {
-    name: "email",
-    placeholder: "Email",
-    selector: selectUserEmail,
-  },
-  {
-    name: "phone",
-    placeholder: "Phone",
-    selector: selectUserName,
-  },
-];
-
 const ProfileForm = () => {
+  const name = useAppSelector(selectUserName);
+  const email = useAppSelector(selectUserEmail);
   return (
     <>
       <h3 className="text-zinc-800 text-base md:text-lg font-bold leading-tight md:leading-normal mb-5">
@@ -27,19 +11,27 @@ const ProfileForm = () => {
       </h3>
 
       <form className="w-full flex flex-col gap-2.5">
-        {inputs.map(({ name, placeholder, selector }) => {
-          const value = useAppSelector(selector);
-          return (
-            <input
-              key={name}
-              className="bg-transparent p-3 lg:w-full border border-neutral-800 border-opacity-20 md:py-4 text-neutral-800 text-sm md:text-base font-medium leading-none md:leading-tight outline-none focus:border-accent focus:border-opacity-1 transition-all duration-300"
-              type="text"
-              name={name}
-              defaultValue={value}
-              placeholder={placeholder}
-            />
-          );
-        })}
+        <input
+          className="bg-transparent p-3 lg:w-full border border-neutral-800 border-opacity-20 md:py-4 text-neutral-800 text-sm md:text-base font-medium leading-none md:leading-tight outline-none focus:border-accent focus:border-opacity-1 transition-all duration-300"
+          type="text"
+          name="name"
+          defaultValue={name}
+          placeholder="Name"
+        />
+        <input
+          className="bg-transparent p-3 lg:w-full border border-neutral-800 border-opacity-20 md:py-4 text-neutral-800 text-sm md:text-base font-medium leading-none md:leading-tight outline-none focus:border-accent focus:border-opacity-1 transition-all duration-300"
+          type="text"
+          name="email"
+          defaultValue={email}
+          placeholder="Email"
+        />
+        <input
+          className="bg-transparent p-3 lg:w-full border border-neutral-800 border-opacity-20 md:py-4 text-neutral-800 text-sm md:text-base font-medium leading-none md:leading-tight outline-none focus:border-accent focus:border-opacity-1 transition-all duration-300"
+          type="text"
+          name="phone"
+          defaultValue={"value"}
+          placeholder="Phone"
+        />
       </form>
     </>
   );
