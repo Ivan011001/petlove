343d602021908ab71b17162f33dc3215273aa0a7
georgijbudik <georgijbudik@gmail.com>
2024-02-17 15:54:45 +0100

    add registration redux

--- state/auth/authSlice.ts
+++ state/auth/authSlice.ts
@@ -1,12 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
+import { signup } from "./authOperations";
 
 export interface IAuthState {
+  name: string;
+  email: string;
+  token: string;
+  imgURL: string;
   isLoggedIn: boolean;
+  error: string | unknown | null;
+  isLoading: boolean;
 }
 
 const initialState: IAuthState = {
+  name: "",
+  email: "",
+  token: "",
+  imgURL: "",
   isLoggedIn: false,
+  error: null,
+  isLoading: false,
 };
 
 const authSlice = createSlice({
@@ -16,6 +29,19 @@ const authSlice = createSlice({
     setAuthState: (state, action: PayloadAction<boolean>) => {
       state.isLoggedIn = action.payload;
     },
+  },
+  extraReducers(builder) {
+    builder.addCase(signup.fulfilled, (state, action) => {
+      state.token = action.payload.accessToken;
+      state.isLoggedIn = true;
+      state.isLoading = false;
+    });
+
+    builder.addCase(signup.rejected, (state, action) => {
+      state.isLoggedIn = false;
+      state.isLoading = false;
+      state.error = action.payload;
+    });
   },
 });
 
