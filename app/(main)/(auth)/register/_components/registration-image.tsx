df1a6ff7ef7f01af85017e23def4e703e4fb431d
georgijbudik <georgijbudik@gmail.com>
2024-02-18 20:20:19 +0100

    fixed register image

--- app/(main)/(auth)/register/_components/registration-image.tsx
+++ app/(main)/(auth)/register/_components/registration-image.tsx
@@ -2,19 +2,20 @@ import Image from "next/image";
 
 const RegistrationImage = () => {
   return (
-    <div
-      className="w-full h-full bg-center-top bg-cover bg-no-repeat relative md:overflow-hidden"
-      style={{
-        backgroundImage:
-          "url(https://cdn.animaapp.com/projects/65c4c76946e5c0edf183ef9a/releases/65c4c80c7fc157c12e418353/img/rectangle-4561.svg)",
-      }}
-    >
-      <div
-        className="flex items-end p-8 lg:pl-[61px] lg:pb-[97px] absolute md:right-[90px] lg:bg-bottom bg-cover md:float-end md:w-[364px] md:h-[302px] lg:bg-auto bg-no-repeat w-full h-full lg:h-[654px] lg:bottom-0"
-        style={{
-          backgroundImage:
-            "url(https://cdn.animaapp.com/projects/65c4c76946e5c0edf183ef9a/releases/65c4c80c7fc157c12e418353/img/telegram-cloud-document-2-5460905295997845524-2.png)",
-        }}
+    <div className="w-full h-full relative overflow-hidden">
+      <Image
+        className="w-[332px] h-[414px] top-[22px] left-[13px] object-contain absolute md:w-[572px] md:h-[715px] md:left-[106px] lg:left-10"
+        src="https://cdn.animaapp.com/projects/65c4c76946e5c0edf183ef9a/releases/65c4c80c7fc157c12e418353/img/rectangle-4561.svg"
+        alt="Rectangle"
+        width={332}
+        height={414}
+      />
+      <Image
+        className="w-[335px] h-[280px] object-cover absolute md:w-[364px] md:h-[302px] md:left-[250px] md:object-cover lg:h-[660px] lg:w-[512px] lg:left-10 lg:bottom-0"
+        src="https://cdn.animaapp.com/projects/65c4c76946e5c0edf183ef9a/releases/65c4c80c7fc157c12e418353/img/telegram-cloud-document-2-5460905295997845524-2.png"
+        alt="Cat"
+        width={335}
+        height={280}
       />
       <div className="hidden md:flex gap-2 items-start bg-white rounded-3xl w-[294px] p-4 md:absolute md:bottom-8 md:left-8 lg:left-[65px] lg:bottom-[106px]">
         <div className="flex-shrink rounded-full w-[60px] h-[60px] bg-yellow-50 flex justify-center items-center p-4">
