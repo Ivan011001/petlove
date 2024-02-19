fcad6afae33cc5eea7c12ad5fc14ee28a84c12df
georgijbudik <georgijbudik@gmail.com>
2024-02-19 20:17:05 +0100

    added favorites and viewed list

--- app/(main)/profile/_components/viewed-list.tsx
+++ app/(main)/profile/_components/viewed-list.tsx
@@ -1,5 +1,21 @@
+import { selectUserViewed } from "@/state/auth/authSelectors";
+import { useAppSelector } from "@/state/hooks";
+import NoticesItem from "../../(public)/notices/_components/notices-item";
+
 const ViewedList = () => {
-  return <div>ViewedList</div>;
+  const viewed = useAppSelector(selectUserViewed);
+
+  return (
+    <ul className="flex flex-col md:flex-row md:flex-wrap items-center justify-center md:gap-5 lg:justify-start lg:gap-6">
+      {viewed?.map((viewed) => {
+        return (
+          <li key={viewed.id} className="md:w-[342px] lg:w-[320px]">
+            <NoticesItem item={viewed} />
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
 export default ViewedList;
