5d885366cbea6d8fc1fda12e604dcd82b763034a
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-02-20 14:00:15 +0200

    added loaders

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,5 +1,5 @@
 const HomeLayout = ({ children }: { children: React.ReactNode }) => {
-  return <main className="container flex flex-col py-5">{children}</main>;
+  return <main className="container flex flex-col pb-5">{children}</main>;
 };
 
 export default HomeLayout;
